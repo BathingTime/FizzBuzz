e8b579239c6d599fdea9cc83c89182aa8e9262a0
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-09-09 17:05:01 -0400

    Moved the other test files to lab1.

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -19,7 +19,7 @@ public class Multiples {
         return count;
     }
 
-    public static int multiple() {
+    public static int multiples() {
         int count = 0;
 
         for (int i = 1; i < 1000; i++) {
